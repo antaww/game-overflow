eac85f8f4241e12c9e957adbfc974fa7d0243b30
xhmyjae <tomatis.mt@gmail.com>
2022-03-21 16:29:42 +0100

    feat: add creation user function

--- backend/sql/go.mod
+++
@@ -1,3 +0,0 @@
-module sql
-
-go 1.17
--- backend/sql/index.go
+++ backend/sql/index.go
@@ -2,27 +2,36 @@ package sql
 
 import (
 	"database/sql"
+	"fmt"
 	"time"
 )
 
-var db *sql.DB
+var DB *sql.DB
 
 
 type User struct {
 	ID int64
 	Username string
+	IsOnline bool
 	Password string
 	Email string
 	Locale string
 	ProfilePic string
 	Description string
-	Creation time.Time
+	CreationDate time.Time
 	Role string
 }
 
 
 func SaveUser(SavedUser User) (int64, error) {
-	result, err := db.Exec("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)", SavedUser.ID, SavedUser.Username, SavedUser.Password, SavedUser.Email, SavedUser.Locale, SavedUser.ProfilePic, SavedUser.Description, SavedUser.Creation, SavedUser.Role)
-
+	result, err := DB.Exec("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", SavedUser.ID,  SavedUser.Username, SavedUser.IsOnline, SavedUser.Password, SavedUser.Email, SavedUser.Locale, SavedUser.ProfilePic, SavedUser.Description, SavedUser.CreationDate, SavedUser.Role)
+	if err != nil {
+		return 0, fmt.Errorf("SaveUser error: %v", err)
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("SaveUser error: %v", err)
+	}
+	return id, nil
 }
 
