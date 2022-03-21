1c0b2edf581f51b72d1bdddf0ea0ae6bf3341761
antaww <anta.pizzetta@gmail.com>
2022-03-21 12:05:07 +0100

    css login page working

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func main(){
-	templ := template.Must(template.ParseFiles("index.gohtml")) //define html file
+func main() {
+	templ, err := template.New("").ParseGlob("../templates/*.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fs := http.FileServer(http.Dir("main.css")) //define css file
-	http.Handle("/static/", http.StripPrefix("/static/", fs)) //set css file to static
+	css := http.FileServer(http.Dir("../style/"))              //define css file
+	http.Handle("/static/", http.StripPrefix("/static/", css)) //set css file to static
+
+	resources := http.FileServer(http.Dir("../resources/"))                //define css file
+	http.Handle("/resources/", http.StripPrefix("/resources/", resources)) //set css file to static
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := templ.ExecuteTemplate(w, "index.gohtml", nil)
+		path := strings.TrimPrefix(r.URL.Path, "/")
+		if path == "favicon.ico" {
+			return
+		}
+		if path == "" {
+			err := templ.ExecuteTemplate(w, "index.gohtml", nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		err := templ.ExecuteTemplate(w, "login.gohtml", nil)
 		if err != nil {
 			log.Fatal(err)
 		}
 	})
 
-	err := http.ListenAndServe(":8091", nil)
+	fmt.Println("Server started at localhost:8091")
+	err = http.ListenAndServe(":8091", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
