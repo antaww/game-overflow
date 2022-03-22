be126a5ca18e952b8f7a6e9735c5cd9077c93153
antaww <anta.pizzetta@gmail.com>
2022-03-22 16:30:31 +0100

    login page working (need redirect if exists = true)

--- backend/main.go
+++ backend/main.go
@@ -17,13 +17,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	css := http.FileServer(http.Dir("../client/style/"))              //define css file
+	css := http.FileServer(http.Dir("../client/style/"))       //define css file
 	http.Handle("/static/", http.StripPrefix("/static/", css)) //set css file to static
 
-	resources := http.FileServer(http.Dir("../backend/resources/"))                //define css file
+	resources := http.FileServer(http.Dir("../backend/resources/"))        //define css file
 	http.Handle("/resources/", http.StripPrefix("/resources/", resources)) //set css file to static
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("route / request")
 		path := strings.TrimPrefix(r.URL.Path, "/")
 		if path == "favicon.ico" {
 			return
@@ -42,6 +43,30 @@ func main() {
 		err := templ.ExecuteTemplate(w, "login.gohtml", nil)
 		if err != nil {
 			log.Fatal(err)
+		}
+
+		if r.Method == "POST" {
+			err := r.ParseForm()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			exists, err := UserLogin(
+				r.FormValue("username"),
+				r.FormValue("password"),
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if exists {
+				fmt.Println("REDIRECT")
+				http.Redirect(w, r, "/", http.StatusOK)
+				fmt.Println("apres")
+				return
+			}
+
+			return
 		}
 	})
 
@@ -71,12 +96,37 @@ func main() {
 
 	})
 
+	http.HandleFunc("/editusername", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("edit username page loaded")
+		err := templ.ExecuteTemplate(w, "edit_username.gohtml", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if r.Method == "POST" {
+			err := r.ParseForm()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			err = EditUsername(
+				r.FormValue("oldusername"),
+				r.FormValue("newusername"),
+			)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+	})
+
 	// Capture connection properties.
 	cfg := mysql.Config{
-		User:                 "xhmyjae",
-		Passwd:               "xhmyjae",
+		User:                 "root",
+		Passwd:               "",
 		Net:                  "tcp",
-		Addr:                 "10.13.33.123:3306",
+		Addr:                 "",
 		DBName:               "forum",
 		AllowNativePasswords: true,
 	}
@@ -92,10 +142,10 @@ func main() {
 	}
 	fmt.Println("Connected!")
 
+	fmt.Println("Server started at localhost:8091")
 	err = http.ListenAndServe(":8091", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Server started at localhost:8091")
 
 }
