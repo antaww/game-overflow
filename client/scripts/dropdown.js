ede4e598b5841dbc50734aa83be0cf15098ea527
antaww <anta.pizzetta@gmail.com>
2022-03-28 09:37:11 +0200

    categories dropdown base done

--- client/scripts/dropdown.js
+++ client/scripts/dropdown.js
@@ -1,7 +1,13 @@
 const username = document.querySelector(".username")
 const dropdown = document.querySelector(".dropdown")
+const categories = document.querySelector(".categories")
 
 username.addEventListener('click', event => {
     console.log("username clicked")
+    dropdown.classList.toggle("block")
+})
+
+categories.addEventListener('click', event => {
+    console.log("categories clicked")
     dropdown.classList.toggle("block")
 })
