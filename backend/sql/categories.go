06b1f2e1fe63d461a92a1a6b5043a78512616439
Ayfri <pierre.ayfri@gmail.com>
2022-04-12 17:45:32 +0200

    fix: Fix create-topic hard-coded categories.

---
+++ backend/sql/categories.go
@@ -0,0 +1,27 @@
+package sql
+
+type Category struct {
+	Name string `db:"category_name"`
+	Icon string `db:"icon"`
+}
+
+func GetCategories() ([]Category, error) {
+	var categories []Category
+	row, err := DB.Query("SELECT * FROM categories ORDER BY category_name")
+	if err != nil {
+		return nil, err
+	}
+
+	for row.Next() {
+		var category Category
+		err = row.Scan(&category.Name, &category.Icon)
+		if err != nil {
+			return nil, err
+		}
+		categories = append(categories, category)
+	}
+
+	HandleSQLErrors(row)
+
+	return categories, nil
+}
