3594ef092f254cee63d795fb34ce49eb71d8aef2
antaww <anta.pizzetta@gmail.com>
2022-04-20 17:52:31 +0200

    close modal with esc + modal animation

--- client/scripts/modal.js
+++ client/scripts/modal.js
@@ -2,6 +2,55 @@ const modals = document.querySelectorAll('.myModal');
 const btn = document.querySelectorAll('.modalProfileBtn');
 const span = document.querySelectorAll('.close');
 
+
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+	e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+	if (keys[e.keyCode]) {
+		preventDefault(e);
+		return false;
+	}
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+	window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+		get: function () {
+			supportsPassive = true;
+		}
+	}));
+} catch (e) {
+}
+
+var wheelOpt = supportsPassive ? {passive: false} : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+	window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+	window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+	window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+	window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+	window.removeEventListener('DOMMouseScroll', preventDefault, false);
+	window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+	window.removeEventListener('touchmove', preventDefault, wheelOpt);
+	window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+//log everytime window is scrolled
+window.addEventListener('scroll', e => {
+	console.log(window.scrollY);
+});
+
 btn.forEach(element => {
 	element.addEventListener('click', e => {
 		let clicked = e.target;
@@ -9,6 +58,7 @@ btn.forEach(element => {
 			return clicked.parentElement.parentElement.parentElement.querySelector('.topic-user p').innerText === modal.querySelector('.modal-name').innerText;
 		});
 		modal.classList.add('modal-display');
+		disableScroll();
 		console.log('modal opened');
 	});
 });
@@ -17,6 +67,7 @@ span.forEach(element => {
 	element.addEventListener('click', () => {
 		modals.forEach(modal => {
 			modal.classList.remove('modal-display');
+			enableScroll();
 			console.log('modal closed');
 		});
 	});
@@ -27,8 +78,24 @@ document.addEventListener('click', e => {
 		if (modal.classList.contains('modal-display') && !e.target.classList.contains('avatar')) {
 			if (e.target !== modal.querySelector('.modal-content')) {
 				modal.classList.remove('modal-display');
+				enableScroll();
 				console.log('modal closed by outside');
 			}
 		}
 	});
 });
+
+document.addEventListener('keydown', e => {
+	modals.forEach(modal => {
+		if (modal.classList.contains('modal-display')) {
+			if (e.keyCode === 27) {
+				modal.classList.remove('modal-display');
+				enableScroll();
+				console.log('modal closed from esc');
+			}
+		}
+	})
+});
+
+
+
