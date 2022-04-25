bb2a01c833a87c93ad0174519fca1b0a10187aaf
xhmyjae <tomatis.mt@gmail.com>
2022-04-25 16:01:23 +0200

    fit: toggle class on edit commment + online circle size

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -67,7 +67,7 @@ function editMessage() {
             const text = id.querySelector("p.posts-content");
             // set it to modifiable
             text.setAttribute("contenteditable", "false");
-            clicked.querySelector('.send-edited-form').querySelector('.send-edited-comment').classList.toggle('no-display');
+            clickedParent.querySelector('.edit-comment').querySelector('.fa-solid').classList.toggle('no-display');
             clicked.classList.toggle('no-display');
 
             const messageId = e.target.closest('.send-edited-form').getAttribute('IdMessage');
