2ce8d3adc97dfff7c3d69e26b7dd3661810b52ba
Ayfri <pierre.ayfri@gmail.com>
2022-05-20 06:11:45 +0200

    feat: Improve create-topic button, fixe more css issues, fix again footer.

--- client/scripts/feed.js
+++ client/scripts/feed.js
@@ -1,11 +1,25 @@
 window.addEventListener('load', () => {
-	const select = document.querySelector('#feed-sorting');
+    const select = document.querySelector('#feed-sorting');
 
-	select?.addEventListener('change', () => {
-		const value = select.value;
-		const url = new URL(window.location.href);
-		url.searchParams.set('s', value);
+    select?.addEventListener('change', () => {
+        const value = select.value;
+        const url = new URL(window.location.href);
+        url.searchParams.set('s', value);
 
-		window.location.href = url.href;
-	});
+        window.location.href = url.href;
+    });
+
+    const createTopic = document.querySelector('.create-topic');
+	if (!createTopic) return;
+    window.addEventListener('scroll', (e) => {
+		const limit = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+		const scrollPosition = window.scrollY;
+		console.log(limit, scrollPosition);
+
+        if (scrollPosition >= limit - 150) {
+            createTopic.style.bottom = `${230 + (scrollPosition - limit)}px`;
+        } else {
+            createTopic.style.bottom = '5rem';
+        }
+    });
 });
